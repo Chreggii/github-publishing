bc65e1d63e969916cedefa5504e7b41fdb43f194
Christian Omlin <christian.omlin@siemens.com>
2023-01-27 21:56:48 +0100

    chore: try to consume env variable in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { environment } from '../environments/environment.onprem';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor() {
+    console.log(environment.envVar.test);
+  }
+
   title = 'github-publishing';
 }
